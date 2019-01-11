f0f51e7b031c25bb5d664f064061cb8e192d9422
harpreetvirkk <harpreetvirk2018@gmail.com>
2019-01-11 16:16:49 +0530

    solutions

---
+++ P2_EvenFibonacciNumbers.py
@@ -0,0 +1,11 @@
+fib=[]
+fib.append(int(1))
+fib.append(int(2))
+sum=0
+while fib[len(fib)-1]<4000000:
+    fib.append(int(fib[len(fib)-1])+int(fib[len(fib)-2]))
+fib.pop(len(fib)-1)
+for i in range(len(fib)):
+    if fib[i]%2==0:
+        sum=sum+fib[i]
+print(sum)
